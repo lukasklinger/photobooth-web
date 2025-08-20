3474ef82ce4d9abb93dadd6a515490be49a45f00
Lukas <lukas@lukasklinger.com>
2025-08-20 16:24:15 +0200

    add 1 as key

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -1,6 +1,6 @@
 function registerKeyboard() {
     document.body.onkeyup = function (e) {
-        if (e.key == " " || e.code == "Space") {
+        if (e.key == " " || e.code == "Space" || e.key == "1") {
             start()
         }
     }
